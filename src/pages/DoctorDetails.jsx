0fd192266507d2fd58001671d1aea9a049dfca0c
AARTI SINGH <147161209+an04jali@users.noreply.github.com>
2026-03-02 20:05:57 +0530

    Fixed DoctorDetails syntax issue

--- src/pages/DoctorDetails.jsx
+++ src/pages/DoctorDetails.jsx
@@ -8,10 +8,15 @@ const DoctorDetails = () => {
 
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const timeSlots = [
-    "9:00 AM","10:00 AM","11:00 AM","12:00 PM",
-    "1:00 PM","2:00 PM","5:00 PM","6:00 PM",
-    "7:00 PM","8:00 PM","9:00 PM"
-  ];
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "2:00 PM",
+  "5:00 PM",
+  "6:00 PM",
+  "7:00 PM",
+  "8:00 PM"
+];
 
   const handleBookAppointment = () => {
     if (!selectedDay || !selectedTime) {
